520cf84666a15a0efa5fd2fa30e62fa3f7dc1b87
shilpashingnapure <shingnapureshilpa2@gmail.com>
2023-12-29 15:51:03 +0530

    new portfolio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { WorkExperienceComponent } from './work-experience/work-experience.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { IntroComponent } from './intro/intro.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { TechStackComponent } from './tech-stack/tech-stack.component';
+import {MatIconModule} from '@angular/material/icon';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import {MatTabsModule} from '@angular/material/tabs';
+import { MiniProjectsComponent } from './projects/mini-projects/mini-projects.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PortfolioComponent,
+    WorkExperienceComponent,
+    ContactsComponent,
+    IntroComponent,
+    ProjectsComponent,
+    TechStackComponent,
+    MiniProjectsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatIconModule,
+    BrowserAnimationsModule ,
+    MatTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/portfolio/portfolio.component.ts
@@ -0,0 +1,25 @@
+import { Component, HostListener, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-portfolio',
+  templateUrl: './portfolio.component.html',
+  styleUrls: ['./portfolio.component.scss']
+})
+export class PortfolioComponent implements OnInit {
+
+  constructor() { }
+
+  scrolled : boolean = false;
+
+  
+
+  @HostListener("window:scroll" , [])
+  onWindowScroll(){
+    this.scrolled = window.scrollY > 0;
+    
+  }
+
+  ngOnInit(): void {
+  }
+
+}
