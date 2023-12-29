ecf71fec60bb0506af67d4005d507a9af0a2dd6f
shilpashingnapure <shingnapureshilpa2@gmail.com>
2023-12-30 01:04:34 +0530

    added theme and header

---
+++ src/app/portfolio/home-name/home-name.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-name',
+  templateUrl: './home-name.component.html',
+  styleUrls: ['./home-name.component.scss']
+})
+export class HomeNameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
